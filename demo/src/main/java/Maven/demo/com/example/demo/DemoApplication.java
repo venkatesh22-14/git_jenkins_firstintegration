157fe7cdd23b524af0b2c3d3f6682300f2c4a969
venkatesh22-14 <84233087+venkatesh22-14@users.noreply.github.com>
2021-12-15 17:42:03 +0530

    Add files via upload

---
+++ demo/src/main/java/Maven/demo/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package Maven.demo.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
